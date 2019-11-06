0332f77df39c09ca18a39893ffdf20f0aaaa581d
Jacob Ferrero <jacobaferrero@gmail.com>
2019-11-05 20:10:07 -0600

    Set up Ktor server

---
+++ fantasydating/src/main/java/com/queatz/fantasydating/Backend.kt
@@ -0,0 +1,45 @@
+package com.queatz.fantasydating
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.Compression
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.gson.gson
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import java.text.DateFormat
+import java.time.LocalDate
+
+data class Model(val name: String, val items: List<Item>, val date: LocalDate = LocalDate.of(2018, 4, 13))
+data class Item(val key: String, val value: String)
+
+val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(Compression)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        gson {
+            setDateFormat(DateFormat.LONG)
+            setPrettyPrinting()
+        }
+    }
+    routing {
+        get("/v1") {
+            call.respond(model)
+        }
+        get("/v1/item/{key}") {
+            val item = model.items.firstOrNull { it.key == call.parameters["key"] }
+            if (item == null)
+                call.respond(HttpStatusCode.NotFound)
+            else
+                call.respond(item)
+        }
+    }
+}
