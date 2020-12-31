78a64fde7d536d466c504a5027e780a6c72a9d9e
Jacob Ferrero <jacobaferrero@gmail.com>
2020-12-31 14:16:29 -0600

    Fix query

--- fantasydating/src/main/java/com/queatz/fantasydating/Query.kt
+++ fantasydating/src/main/java/com/queatz/fantasydating/Query.kt
@@ -76,7 +76,7 @@ object AqlQuery {
                         FILTER edge.kind == 'love' AND personLove._id == @person RETURN true
                 ) != 0,
                 styles: (FOR personStyle, edge IN OUTBOUND person GRAPH @graph
-                        FILTER edge.kind == 'link' AND personStyle.kind == 'style' SORT POSITION(person.stylesOrder || [], personStyle._key) ASC RETURN personStyle)
+                        FILTER edge.kind == 'link' AND personStyle.kind == 'style' SORT POSITION(person.stylesOrder || [], personStyle._key, true) ASC RETURN personStyle)
             }
         )
     """
