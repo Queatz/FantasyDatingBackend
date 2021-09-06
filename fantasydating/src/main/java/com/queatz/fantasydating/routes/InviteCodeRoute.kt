959cdb7fc36adc7d22cd2b088470094aa5ecbbb9
Jacob A. Ferrero <jacobaferrero@gmail.com>
2021-09-06 15:43:55 -0500

    Allow self invites

--- fantasydating/src/main/java/com/queatz/fantasydating/routes/InviteCodeRoute.kt
+++ fantasydating/src/main/java/com/queatz/fantasydating/routes/InviteCodeRoute.kt
@@ -11,6 +11,9 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 
 class InviteCodeRoute constructor(private val on: On) {
+
+    private val areSelfInvitesSupported = true
+
     suspend fun post(call: ApplicationCall) {
         call.parameters["id"]?.let {
             val me = on<Me>().person
@@ -20,28 +23,35 @@ class InviteCodeRoute constructor(private val on: On) {
                 return
             }
 
-            val invite = on<Db>().getInviteCode(it)
+            if (it == "self") {
+                if (!areSelfInvitesSupported) {
+                    call.respond(SuccessResponse(false, "You\'ll need an invite code from someone else to join at this time."))
+                    return
+                }
+            } else {
+                val invite = on<Db>().getInviteCode(it)
 
-            if (invite == null) {
-                call.respond(HttpStatusCode.NotFound)
-                return
-            }
+                if (invite == null) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return
+                }
 
-            if (invite.used) {
-                call.respond(SuccessResponse(false, "That invite code has already been used"))
-                return
-            }
+                if (invite.used) {
+                    call.respond(SuccessResponse(false, "That invite code has already been used"))
+                    return
+                }
 
-            invite.used = true
-            invite.usedBy = me.id!!
-            on<Arango>().save(invite)
+                invite.used = true
+                invite.usedBy = me.id!!
+                on<Arango>().save(invite)
 
-            on<Db>().getById(invite.person, Person::class)?.let { inviter ->
-                val event = Event()
-                event.name = "You were invited by ${inviter.name}"
-                event.person = me.id!!
-                event.data = on<Json>().to(InvitedEventType(inviter.id!!))
-                on<Arango>().save(event)
+                on<Db>().getById(invite.person, Person::class)?.let { inviter ->
+                    val event = Event()
+                    event.name = "You were invited by ${inviter.name}"
+                    event.person = me.id!!
+                    event.data = on<Json>().to(InvitedEventType(inviter.id!!))
+                    on<Arango>().save(event)
+                }
             }
 
             me.invited = true
